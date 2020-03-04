25edf36c2cf6ac860a694b9a49ef84531b885d92
computer <smithmark.ms3@gmail.com>
2020-03-04 15:07:18 +0000

    Profile picture black and white colour added

---
+++ main.js
@@ -0,0 +1,8 @@
+function NormalPic() {
+	document.getElementById("profilePicture").src = "image/mark.png";
+}
+
+function BlackAndWhitePic() {
+	document.getElementById("profilePicture").src =
+		"image/mark-black-and-white.png";
+}
