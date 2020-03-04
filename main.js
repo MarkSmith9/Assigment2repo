31b706f5aaa17f176b1b6444fbb68fd0b84f768f
computer <smithmark.ms3@gmail.com>
2020-03-04 16:15:06 +0000

    added comments

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-function NormalPic() {
-	document.getElementById("profilePicture").src = "image/mark.png";
-}
+//The material in class was used as the guide to make the picture change colour when an element is hovered over
 
+//this function changes the profile picture to black and white
 function BlackAndWhitePic() {
 	document.getElementById("profilePicture").src =
 		"image/mark-black-and-white.png";
+}
+
+//this function changes the profile picture back to normal
+function NormalPic() {
+	document.getElementById("profilePicture").src = "image/mark.png";
 }
