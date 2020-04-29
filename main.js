098365ed9c12d6d5118d3529859ee5d3f22aeb8d
MarkSmith9 <smithmark.ms3@gmail.com>
2020-04-29 10:40:57 +0100

    a

--- main.js
+++ main.js
@@ -11,4 +11,22 @@ function NormalPic() {
 	document.getElementById("profilePicture").src = "image/mark.png";
 }
 
-function HideLoader() {}
+function FormSubmit() {
+	alert("Thank you for submitting"); //informs the user that the form has been submitted
+}
+
+function clock() { //function with a real time clock
+	let today = new Date();
+	let seconds = today.getSeconds(); //gets seconds
+	let minutes = today.getMinutes(); //gets minutes
+	let hours = today.getHours();     //gets hours
+	seconds = timeCheck(seconds);
+	minutes = timeCheck(minutes);
+	document.getElementById('clock').innerHTML =
+	hours + " : " + minutes + " : " + seconds; //displays hours minutes and seconds of the clock, separated by semicolons and spaces
+	let t = setTimeout(clock, 500);
+  }
+  function timeCheck(i) {
+	if (i < 10) {i = "0" + i};  
+	return i; // if number is less than 10 adds a 0 in front of it
+  }
